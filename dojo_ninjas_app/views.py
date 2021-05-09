312353b63ad306686a75c20172ed6ada2155f822
Ramon O. Mallari Jr <ramon.mallari@gmail.com>
2021-05-09 15:31:29 -0700

    First Commit

---
+++ dojo_ninjas_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import Dojo, Ninja
+
+# Create your views here.
+
+def index(request):
+    context = {
+        "all_the_dojos": Dojo.objects.all(),
+    }
+    return render(request, 'index.html', context)
+
+def add_dojo(request):
+    Dojo.objects.create(
+        name=request.POST['name'],
+        city=request.POST['city'],
+        state=request.POST['state'],
+    )
+    return redirect('/')
+
+def add_ninja(request):
+    Ninja.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        dojo_id=request.POST['dojo']
+    )
+    return redirect('/')
+
